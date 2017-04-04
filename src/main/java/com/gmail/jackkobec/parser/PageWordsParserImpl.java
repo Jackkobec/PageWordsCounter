d94b1abeaf498075dab773e11a0bbd7487f23171
jackkobec <jakkobec@gmail.com>
2017-04-04 07:20:31 +0300

    Some regex. Worked tug killer and Tokenizer.

--- src/main/java/com/gmail/jackkobec/parser/PageWordsParserImpl.java
+++ src/main/java/com/gmail/jackkobec/parser/PageWordsParserImpl.java
@@ -16,6 +16,10 @@ public class PageWordsParserImpl implements PageWordsParser {
         StringBuilder stringBuilder = new StringBuilder();
         ArrayList<String> allWords = new ArrayList<>();
 //        StringTokenizer stringTokenizer = new StringTokenizer(string, " \t\n\r,.");
+
+        string = someTagsContentKiller(string);
+        string = htmlTagKiller(string);
+
         StringTokenizer stringTokenizer = new StringTokenizer(string, " \t\n\r,.:-;/<>");
 
         while (stringTokenizer.hasMoreTokens()) {
@@ -32,12 +36,50 @@ public class PageWordsParserImpl implements PageWordsParser {
             }
         }
         System.out.println(allWords);
+        System.out.println(allWords.size());
+
         return stringBuilder.toString();
     }
 
+    /**
+     * Select only words consisting on letters
+     *
+     * @param word
+     * @return
+     */
     private boolean checkOnlyLetters(String word) {
         String ONLY_LETTERS_PATTERN = "[A-Za-z]*";
 
         return Pattern.compile(ONLY_LETTERS_PATTERN).matcher(word).matches();
+    }
+
+    /**
+     * Replaces all HTML tags with spaces
+     *
+     * @param string
+     * @return
+     */
+    private String htmlTagKiller(String string) {
+
+        return string.replaceAll("<[a-zA-Z\\s/]+>", " ");
+    }
+
+    /**
+     * Replaces some HTML tags with spaces
+     *
+     * @param string
+     * @return
+     */
+    private String someTagsContentKiller(String string) {
+
+        string = string.replaceAll("<style.*?>.*?</style>", " ");
+
+        string = string.replaceAll("<script.*?>.*?</script>", " ");
+
+        string = string.replaceAll("<img.*?/>.*?", " ");
+
+        string = string.replaceAll("<object.*?>.*?</object>", " ");
+
+        return string;
     }
 }
